7607fb2342b8fa554cc07ebb22d31a236fa8dcb1
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-09-22 20:51:25 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position posicao = new Position(2,5);
+        System.out.println(posicao.toString());
+        
+    }
+}
