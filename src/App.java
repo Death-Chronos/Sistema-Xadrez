9157f7df87b1b7d5d609cef63cb57438220f4a7b
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-09-22 21:38:57 -0300

    Escrevendo o tabuleiro no terminal

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import boardgame.Board;
-import boardgame.Position;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Board tabuleiro = new Board(8,8);
-
-    }
-}
