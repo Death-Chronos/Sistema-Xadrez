75abc1fd055c48a789ee3b07612647198e92749d
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-11-09 17:42:38 -0300

    Peão

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -194,9 +195,11 @@ public class ChessMatch {
 
     private void initalSetup() {
         placeNewPiece('h', 7, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new Pawn(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
 
+        
+        placeNewPiece('c', 8, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new King(board, Color.BLACK));
 
